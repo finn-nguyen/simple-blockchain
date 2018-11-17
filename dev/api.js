3f90ad42640b005acaf89219f8cee4c4e30fd8c8
nvtuongcs <nvtuongcs@gmail.com>
2018-11-17 15:01:24 +0700

    Add api endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,43 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const Blockchain = require('./blockchain')
+const uuid = require('uuid/v1')
+const nodeAddress = uuid().split('-').join('')
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+const bitcoin = new Blockchain()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/blockchain', (req, res) => {
+  res.send(bitcoin)
 })
 
-app.listen(3000)
+app.post('/transaction', (req, res) => {
+  const { amount, sender, recipient } = req.body
+  const blockIndex = bitcoin.createNewTransaction(amount, sender, recipient)
+  res.json({ note: `Transaction will be added in block ${blockIndex}`})
+})
+
+app.get('/mine', (req, res) => {
+  const lastBlock = bitcoin.getLastBlock()
+  const previousBlockHash = lastBlock['hash']
+  const currentBlockData = {
+    transactions: bitcoin.pendingTransactions,
+    index: lastBlock['index'] + 1
+  }
+  const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData)
+  const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce)
+  console.log(blockHash)
+  bitcoin.createNewTransaction(1.5, '00', nodeAddress)
+  const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash)
+  res.json({
+    note: 'New block mined successfully',
+    block: newBlock
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
